24e001a299c09bc5bfd318b96da6d18212bd8aeb
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2018-01-10 22:09:56 +0900

    use Repro instead of Clicktale

--- app/src/main/java/io/github/yusukeiwaki/clicktalehideviewbug/SampleApplication.java
+++ app/src/main/java/io/github/yusukeiwaki/clicktalehideviewbug/SampleApplication.java
@@ -1,27 +1,15 @@
 package io.github.yusukeiwaki.clicktalehideviewbug;
 
 import android.app.Application;
-import android.util.Log;
 
-import com.clicktale.clicktalesdk.Clicktale;
-import com.clicktale.clicktalesdk.ClicktaleCallback;
+import io.repro.android.Repro;
 
 public class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 
-        Clicktale.start(this, new ClicktaleCallback() {
-            @Override
-            public void onSessionURLCreated(String s) {
-                Log.d("SampleApplication", "onSessionURLCreated: " + s);
-            }
-
-            @Override
-            public void onSessionIDCreated(String s) {
-                Log.d("SampleApplication", "onSessionIDCreated: " + s);
-            }
-        }, getString(R.string.clicktale_access_key), getString(R.string.clicktale_secret_key));
-        Clicktale.setDevMode(BuildConfig.DEBUG);
+        Repro.setup(this, getString(R.string.repro_app_token));
+        Repro.startRecording();
     }
 }
